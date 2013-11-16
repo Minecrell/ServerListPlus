203f91a2bae1c6640b6926ba3fdb614968d75b64
Minecrell <dev@minecrell.net>
2013-11-16 14:19:57 +0100

    [BREAKING] Rollback some commits
    - License is now GPLv3 again!

--- api/src/main/java/net/minecrell/serverlistplus/api/ServerListPlusAPI.java
+++ api/src/main/java/net/minecrell/serverlistplus/api/ServerListPlusAPI.java
@@ -1,15 +1,21 @@
 package net.minecrell.serverlistplus.api;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.ServerPing.PlayerInfo;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class ServerListPlusAPI {
     private final Path configPath;
@@ -24,8 +30,8 @@ public final class ServerListPlusAPI {
         this.reload();
     }
 
-    public ServerPing handleServerPing(ServerPing ping) {
-        return handleServerPing(ping, lines);
+    public ServerPing handleServerPing(InetAddress address, ServerPing ping) {
+        return handleServerPing(address, ping, lines);
     }
 
     public void reload() throws IOException {
@@ -34,34 +40,37 @@ public final class ServerListPlusAPI {
             Files.createFile(configPath);
             lines = new ArrayList<>();
         } else
-            this.lines = colorize(Files.readAllLines(configPath, StandardCharsets.UTF_8));
+            // Colorize and replace empty lines using an RESET color
+            this.lines = Lists.transform(Files.readAllLines(configPath, StandardCharsets.UTF_8), new Function<String, String>() {
+                @Override
+                public String apply(String s) {
+                    return ChatColor.translateAlternateColorCodes('&', (s.length() > 0) ? s : "&r");
+                }
+            });
     }
 
     public List<String> getLines() {
         return lines;
     }
 
-    public static ServerPing handleServerPing(ServerPing ping, List<String> lines) {
+    private static Map<String, String> playerIPs = new HashMap<>();
+
+    public static ServerPing handleServerPing(InetAddress address, ServerPing ping, List<String> lines) {
         if (lines.size() == 0) {
             ping.getPlayers().setSample(null); return ping;
         }
 
+        final String playerIP = address.getHostAddress();
+
         PlayerInfo[] players = new PlayerInfo[lines.size()];
-        for (int i = 0; i < players.length; i++)
-            players[i] = new PlayerInfo(lines.get(i), "");
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new PlayerInfo(lines.get(i).replace("%player%", (playerIPs.containsKey(playerIP) ? playerIPs.get(playerIP) : "player")), "");
+        }
 
         ping.getPlayers().setSample(players); return ping;
     }
 
-
-    private static String colorize(String message){
-        return message.replaceAll("&([0-9a-fk-or])", "\u00A7$1");
-    }
-
-    private static List<String> colorize(List<String> messages){
-        List<String> newList = new ArrayList<>(messages.size());
-        for (String msg : messages)
-            newList.add(colorize(msg));
-        return newList;
+    public static void handlePlayerLogin(String playerName, InetAddress address) {
+        playerIPs.put(address.getHostAddress(), playerName);
     }
 }
--- bungee/src/main/java/net/minecrell/serverlistplus/ServerListPlus.java
+++ bungee/src/main/java/net/minecrell/serverlistplus/ServerListPlus.java
@@ -1,6 +1,10 @@
 package net.minecrell.serverlistplus;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -10,12 +14,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 
-public class ServerListPlus extends Plugin implements Listener {
+public final class ServerListPlus extends Plugin implements Listener {
+    private File configFile;
     private ServerListPlusAPI serverList;
 
     @Override
     public void onEnable() {
-        File configFile = new File(this.getDataFolder(), "serverlist.txt");
+        this.configFile = new File(this.getDataFolder(), "serverlist.txt");
 
         try {
             this.serverList = new ServerListPlusAPI(configFile);
@@ -25,10 +30,39 @@ public class ServerListPlus extends Plugin implements Listener {
         }
 
         this.getProxy().getPluginManager().registerListener(this, this);
+        this.getProxy().getPluginManager().registerCommand(this, new ReloadCommand());
     }
 
     @EventHandler
     public void onServerPing(ProxyPingEvent event) {
-        serverList.handleServerPing(event.getResponse());
+        serverList.handleServerPing(event.getConnection().getAddress().getAddress(), event.getResponse());
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PostLoginEvent event) {
+        ServerListPlusAPI.handlePlayerLogin(event.getPlayer().getName(), event.getPlayer().getAddress().getAddress());
+    }
+
+    private class ReloadCommand extends Command {
+
+        public ReloadCommand() {
+            super("serverlistplus", "serverlistplus.reload", "serverlist", "slp");
+        }
+
+        @Override
+        public void execute(CommandSender sender, String[] args) {
+            getLogger().info("Reloading configuration...");
+
+            try {
+                serverList.reload();
+
+                getLogger().info("Configuration reloaded!");
+                sender.sendMessage(ChatColor.GREEN + "Configuration successfully reloaded!");
+            } catch (IOException e) {
+                getLogger().log(Level.SEVERE, "Unable to access server list configuration file: " + configFile.getAbsolutePath(), e);
+                getLogger().warning("Cancelling configuration reload!");
+                sender.sendMessage(ChatColor.RED + "An internal error occurred while reloading the configuration!");
+            }
+        }
     }
 }
