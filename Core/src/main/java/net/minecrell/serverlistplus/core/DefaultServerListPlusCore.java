3b0fd90ac17cdbf47352a9e3174e4072d8f05c62
Minecrell <dev@minecrell.net>
2014-02-18 12:05:39 +0100

    Still allow calling the method with none exception.

--- Core/src/main/java/net/minecrell/serverlistplus/core/DefaultServerListPlusCore.java
+++ Core/src/main/java/net/minecrell/serverlistplus/core/DefaultServerListPlusCore.java
@@ -109,8 +109,7 @@ public final class DefaultServerListPlusCore implements ServerListPlusCore {
 
     @Override
     public ServerListPlusException processException(Throwable e, String message) {
-        if (e == null) return null;
-        if (e.getClass() == CoreServerListPlusException.class) return (CoreServerListPlusException) e;
+        if (e != null && e.getClass() == CoreServerListPlusException.class) return (CoreServerListPlusException) e;
         this.getLogger().log(Level.SEVERE, message, e);
         return new CoreServerListPlusException(message, e);
     }
