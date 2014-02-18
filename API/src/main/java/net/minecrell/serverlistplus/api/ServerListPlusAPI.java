b1ba6de0d435dfd59e4ea8a551026f01983a6570
Minecrell <dev@minecrell.net>
2014-02-18 11:05:30 +0100

    Core API things.

--- API/src/main/java/net/minecrell/serverlistplus/api/ServerListPlusAPI.java
+++ API/src/main/java/net/minecrell/serverlistplus/api/ServerListPlusAPI.java
@@ -1,5 +1,5 @@
 /*
- * __                           __ _     _     ___ _
+ *       __                           __ _     _     ___ _
  *      / _\ ___ _ ____   _____ _ __ / /(_)___| |_  / _ \ |_   _ ___
  *      \ \ / _ \ '__\ \ / / _ \ '__/ / | / __| __|/ /_)/ | | | / __|
  *      _\ \  __/ |   \ V /  __/ | / /__| \__ \ |_/ ___/| | |_| \__ \
@@ -24,5 +24,95 @@
 
 package net.minecrell.serverlistplus.api;
 
+import java.net.InetAddress;
+import java.util.logging.Logger;
+
+import net.minecrell.serverlistplus.api.plugin.ServerCommandSender;
+import net.minecrell.serverlistplus.api.plugin.ServerListPlusPlugin;
+
+/**
+ * Representing the core part of the ServerListPlus plugin.
+ * It will handle all server ping requests and is equal in all different implementations of the plugin.
+ */
 public interface ServerListPlusAPI {
+    /**
+     * Gets the name of this plugin core.
+     * @return The plugin core's name.
+     */
+    String getName();
+
+    /**
+     * Gets the version of this plugin core.
+     * @return The plugin core version.
+     */
+    String getVersion();
+
+    /**
+     * Gets the instance of the plugin container running the ServerListPlus plugin.
+     * @return Instance of the plugin container.
+     * @see ServerListPlusPlugin
+     */
+    ServerListPlusPlugin getPlugin();
+
+    /**
+     * Gets the plugin logger used for logging errors and messages in the plugin.
+     * @return The logger of the plugin.
+     */
+    Logger getLogger();
+
+    /**
+     * Processes an exception, sends it to the logger, and finally throws an {@link ServerListPlusException}.
+     * @param e The thrown exception.
+     * @param message The message for logging and the exception.
+     * @return The exception to throw with the specified message and cause.
+     */
+    ServerListPlusException processException(Throwable e, String message);
+
+    /**
+     * Reloads the plugin configuration and disables unnecessary services.
+     * @throws ServerListPlusException If the configuration file could not be successfully read.
+     */
+    void reload() throws ServerListPlusException;
+
+    /**
+     * Handles an server ping request. This will modify the response with the changed data in the plugin and return the
+     * modified response.
+     *
+     * @param client The IP address of the client. (Used for player tracking)
+     * @param response An instance of the server ping wrapper to modify the response.
+     * @see ServerPingResponse
+     */
+    void processRequest(InetAddress client, ServerPingResponse response);
+
+    /**
+     * Handles an server ping request with an specific hostname used to ping the server. This will modify the response
+     * with the changed data in the plugin and return the modified response.
+     *
+     * @param client The IP address of the client. (Used for player tracking)
+     * @param host The hostname used by the client to ping the server.
+     * @param response An instance of the server ping wrapper to modify the response.
+     * @see ServerPingResponse
+     */
+    void processRequest(InetAddress client, String host, ServerPingResponse response);
+
+    /**
+     * Handles the login of a player to the server. This is used by the player tracking to track the players with their IP
+     * address, so it can be used by the {@link #processRequest(InetAddress, ServerPingResponse)} method to create
+     * personalized messages.
+     *
+     * @param playerName The name the player used to login to the server.
+     * @param client The IP address of the client.
+     */
+    void processLogin(String playerName, InetAddress client);
+
+    /**
+     * Handles a command send by a player or the console.
+     *
+     * @param sender The sender of the command, usually a player or the console.
+     * @param cmd The command executed by the player.
+     * @param label The command the player has used - if the command has any aliases.
+     * @param args The arguments the player has used to perform the command.
+     * @see ServerCommandSender
+     */
+    void processCommand(ServerCommandSender sender, String cmd, String label, String[] args);
 }
---
+++ API/src/main/java/net/minecrell/serverlistplus/api/ServerPingResponse.java
@@ -0,0 +1,36 @@
+/*
+ *       __                           __ _     _     ___ _
+ *      / _\ ___ _ ____   _____ _ __ / /(_)___| |_  / _ \ |_   _ ___
+ *      \ \ / _ \ '__\ \ / / _ \ '__/ / | / __| __|/ /_)/ | | | / __|
+ *      _\ \  __/ |   \ V /  __/ | / /__| \__ \ |_/ ___/| | |_| \__ \
+ *      \__/\___|_|    \_/ \___|_| \____/_|___/\__\/    |_|\__,_|___/
+ *                       Customize your server ping!
+ *
+ *  Copyright (C) 2014, Minecrell <https://github.com/Minecrell>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.minecrell.serverlistplus.api;
+
+/**
+ * Represents an completely prepared response on a server ping request to a client.
+ * It is used to wrap the different APIs to have one class for the modifications.
+ * Exact protocol description is available <a href="http://wiki.vg/Server_List_Ping">here</a>.
+ *
+ * @see <a href="http://wiki.vg/Server_List_Ping">Protocol description</a>
+ */
+public interface ServerPingResponse {
+    // TODO: Implement response wrapper
+}
