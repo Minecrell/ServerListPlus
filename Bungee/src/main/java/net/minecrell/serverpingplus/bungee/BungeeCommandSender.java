c4ffab1e712ac5918ba4182de38477c6ba22597d
Minecrell <dev@minecrell.net>
2014-05-18 22:13:17 +0200

    Add basic plugin implementation.

---
+++ Bungee/src/main/java/net/minecrell/serverpingplus/bungee/BungeeCommandSender.java
@@ -0,0 +1,45 @@
+/*
+ *       _____                     _____ _         _____ _
+ *      |   __|___ ___ _ _ ___ ___|  _  |_|___ ___|  _  | |_ _ ___
+ *      |__   | -_|  _| | | -_|  _|   __| |   | . |   __| | | |_ -|
+ *      |_____|___|_|  \_/|___|_| |__|  |_|_|_|_  |__|  |_|___|___|
+ *                                            |___|
+ *  ServerPingPlus - Customize your server ping!
+ *  Copyright (C) 2014, Minecrell <https://github.com/Minecrell>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.minecrell.serverpingplus.bungee;
+
+import net.minecrell.serverpingplus.core.plugin.AbstractCommandSender;
+
+import net.md_5.bungee.api.CommandSender;
+
+public class BungeeCommandSender extends AbstractCommandSender<CommandSender> {
+    public BungeeCommandSender(CommandSender sender) {
+        super(sender);
+    }
+
+    @Override
+    public String getName() {
+        return sender.getName();
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        // Deprecated, but compatibility with older BungeeCord versions
+        sender.sendMessage(message);
+    }
+}
