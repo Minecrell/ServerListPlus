4a51e1dafae9d24669182af775fd7ffb0d77e616
Minecrell <dev@minecrell.net>
2014-06-16 20:40:16 +0200

    This configuration instead.

--- Core/src/main/java/net/minecrell/serverlistplus/core/config/ServerStatusConf.java
+++ Core/src/main/java/net/minecrell/serverlistplus/core/config/ServerStatusConf.java
@@ -49,7 +49,7 @@ public class ServerStatusConf {
         public List<String> Description;
         public PlayersConf Players;
         public VersionConf Version;
-        public FaviconConf Favicon;
+        public List<String> Favicon;
 
         public static class PlayersConf {
             public List<Integer> Online, Max;
@@ -59,12 +59,6 @@ public class ServerStatusConf {
         public static class VersionConf {
             public List<String> Name;
             public Integer Protocol;
-        }
-
-        public static class FaviconConf {
-            public List<String> Files;
-            public List<String> Folders;
-            public List<String> URLs;
         }
     }
 }
