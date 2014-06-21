b44476d9359f3d29ef1392c87ed739a5b00c3561
Minecrell <dev@minecrell.net>
2014-06-21 18:29:21 +0200

    Also fix configuration description.

--- Core/src/main/java/net/minecrell/serverlistplus/core/config/CoreConf.java
+++ Core/src/main/java/net/minecrell/serverlistplus/core/config/CoreConf.java
@@ -28,14 +28,13 @@ import net.minecrell.serverlistplus.core.config.help.Description;
 @Description({
         "WARNING: Changes in this section can possibly damage your server!",
         "PlayerTracking Cache: Change settings here if you want to change the behaviour",
-        "   of the player tracking cache. For example if you set it to '' then it will",
-        "   not delete entries after a player was 2 hours offline from a server.",
-        "   More information: http://goo.gl/oYVk0F"
+        "   of the player tracking cache. Possible settings: http://goo.gl/oYVk0F",
 })
 public class CoreConf {
     public CachesConf Caches = new CachesConf();
 
     public static class CachesConf {
         public String PlayerTracking = "";
+        public String Favicon = "";
     }
 }
