a81bf3a62b89356ec5b0a44c76df77220657d3cc
Minecrell <dev@minecrell.net>
2014-06-22 23:49:50 +0200

    Revert "Revert "Class name is enough - no need for complete package path!""
    This reverts commit 15cabf4b996e10cf511c0ec744000c46b92aaabb.

--- Core/src/main/java/net/minecrell/serverlistplus/core/config/yaml/ConfigurationPropertyUtils.java
+++ Core/src/main/java/net/minecrell/serverlistplus/core/config/yaml/ConfigurationPropertyUtils.java
@@ -43,7 +43,7 @@ public class ConfigurationPropertyUtils extends FieldOrderPropertyUtils {
     public Property getProperty(Class<?> type, String name, BeanAccess bAccess) throws IntrospectionException {
         Property p = super.getProperty(type, name, bAccess);
         if (p instanceof MissingProperty)
-            core.getLogger().warningF("Unknown configuration property: %s @ %s", name, type.getName());
+            core.getLogger().warningF("Unknown configuration property: %s @ %s", name, type.getSimpleName());
         return p;
     }
 }
