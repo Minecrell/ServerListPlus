58605d3feb0469b8f91f60e9a3a02d3791402288
Minecrell <dev@minecrell.net>
2016-03-19 15:41:20 +0100

    Add server configuration description

--- Server/src/main/java/net/minecrell/serverlistplus/server/config/ServerConf.java
+++ Server/src/main/java/net/minecrell/serverlistplus/server/config/ServerConf.java
@@ -1,10 +1,17 @@
 package net.minecrell.serverlistplus.server.config;
 
+import net.minecrell.serverlistplus.core.config.help.Description;
 import net.minecrell.serverlistplus.core.util.Helper;
 
 import java.util.Collections;
 import java.util.List;
 
+@Description({
+        "This section is only for the standalone server implementation of ServerListPlus.",
+        "Address: The address to bind the server to. Can be used to specify the port.",
+        "  - Example: :25565 will bind the server to port 25565",
+        "Login -> Message: The message displayed when the players are kicked from the server."
+})
 public class ServerConf {
     public String Address = "";
     public LoginConf Login = new LoginConf();
