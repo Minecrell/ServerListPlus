7c77236121ba9dcb78c2f3cdf46d971e171127bd
Minecrell <minecrell@minecrell.net>
2018-01-21 11:24:21 +0100

    Use PaperServerListPingEvent when available
    Allows full set of features without ProtocolLib installed.

---
+++ Bukkit/src/main/java/net/minecrell/serverlistplus/bukkit/handlers/PaperEventHandler.java
@@ -0,0 +1,131 @@
+package net.minecrell.serverlistplus.bukkit.handlers;
+
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import net.minecrell.serverlistplus.bukkit.BukkitPlugin;
+import net.minecrell.serverlistplus.core.favicon.FaviconSource;
+import net.minecrell.serverlistplus.core.status.ResponseFetcher;
+import net.minecrell.serverlistplus.core.status.StatusRequest;
+import net.minecrell.serverlistplus.core.status.StatusResponse;
+import net.minecrell.serverlistplus.core.util.Helper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.util.CachedServerIcon;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class PaperEventHandler extends StatusHandler implements Listener {
+
+    private boolean registered;
+
+    public PaperEventHandler(BukkitPlugin plugin) {
+        super(plugin);
+    }
+
+    @EventHandler
+    public void onPaperServerListPing(final PaperServerListPingEvent event) {
+        if (bukkit.getCore() == null) return; // Too early, we haven't finished initializing yet
+
+        StatusRequest request = bukkit.getCore().createRequest(event.getAddress());
+        request.setProtocolVersion(event.getClient().getProtocolVersion());
+        InetSocketAddress host = event.getClient().getVirtualHost();
+        if (host != null) {
+            request.setTarget(host);
+        }
+
+        StatusResponse response = request.createResponse(bukkit.getCore().getStatus(),
+                // Return unknown player counts if it has been hidden
+                new ResponseFetcher() {
+                    @Override
+                    public Integer getOnlinePlayers() {
+                        return event.isHidePlayers() ? null : event.getNumPlayers();
+                    }
+
+                    @Override
+                    public Integer getMaxPlayers() {
+                        return event.isHidePlayers() ? null : event.getMaxPlayers();
+                    }
+
+                    @Override
+                    public int getProtocolVersion() {
+                        return event.getProtocolVersion();
+                    }
+                }
+        );
+
+        // Description
+        String message = response.getDescription();
+        if (message != null) event.setMotd(message);
+
+        // Version name
+        message = response.getVersion();
+        if (message != null) event.setVersion(message);
+        // Protocol version
+        Integer protocol = response.getProtocolVersion();
+        if (protocol != null) event.setProtocolVersion(protocol);
+
+        if (response.hidePlayers()) {
+            event.setHidePlayers(true);
+        } else {
+            // Online players
+            Integer count = response.getOnlinePlayers();
+            if (count != null) event.setNumPlayers(count);
+            // Max players
+            count = response.getMaxPlayers();
+            if (count != null) event.setMaxPlayers(count);
+
+            // Player hover
+            message = response.getPlayerHover();
+            if (message != null) {
+                List<PlayerProfile> profiles = event.getPlayerSample();
+                profiles.clear();
+
+                if (response.useMultipleSamples()) {
+                    count = response.getDynamicSamples();
+                    List<String> lines = count != null ? Helper.splitLinesCached(message, count) :
+                            Helper.splitLinesCached(message);
+
+                    for (String line : lines) {
+                        profiles.add(bukkit.getServer().createProfile(line));
+                    }
+                } else {
+                    profiles.add(bukkit.getServer().createProfile(message));
+                }
+            }
+        }
+
+        // Favicon
+        FaviconSource favicon = response.getFavicon();
+        if (favicon != null) {
+            CachedServerIcon icon = bukkit.getFavicon(favicon);
+            if (icon != null)
+                try {
+                    event.setServerIcon(icon);
+                } catch (UnsupportedOperationException ignored) {}
+        }
+    }
+
+    @Override
+    public boolean register() {
+        if (this.registered) {
+            return false;
+        }
+
+        this.registered = true;
+        bukkit.registerListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean unregister() {
+        if (!this.registered) {
+            return false;
+        }
+
+        this.registered = false;
+        bukkit.unregisterListener(this);
+        return true;
+    }
+
+}
