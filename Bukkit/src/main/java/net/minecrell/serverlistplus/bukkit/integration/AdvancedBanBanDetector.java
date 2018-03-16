b77be4d2bb1cfa194cdca37a8559881c76cf205a
BrainStone <yannick@tedworld.de>
2018-03-16 11:13:59 +0100

    Fixed derp

--- Bukkit/src/main/java/net/minecrell/serverlistplus/bukkit/integration/AdvancedBanBanDetector.java
+++ Bukkit/src/main/java/net/minecrell/serverlistplus/bukkit/integration/AdvancedBanBanDetector.java
@@ -1,4 +1,4 @@
-package net.minecrell.serverlistplus.bungee.integration;
+package net.minecrell.serverlistplus.bukkit.integration;
 
 import me.leoko.advancedban.manager.PunishmentManager;
 import me.leoko.advancedban.utils.Punishment;
