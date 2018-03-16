2e3cdc72b713c40f9a3320a2cd261e6255ca0294
BrainStone <yannick@tedworld.de>
2018-03-16 11:07:35 +0100

    Expanded BanDetector
    Now includes:
    - Ban reason
    - Ban operator
    - Ban expiration

--- src/main/java/net/minecrell/serverlistplus/core/player/ban/BanDetector.java
+++ src/main/java/net/minecrell/serverlistplus/core/player/ban/BanDetector.java
@@ -2,7 +2,43 @@ package net.minecrell.serverlistplus.core.player.ban;
 
 import net.minecrell.serverlistplus.core.player.PlayerIdentity;
 
+import java.sql.Timestamp;
+
 public interface BanDetector {
 
+    /**
+     * Checks if a player is banned.
+     * 
+     * @param playerIdentity Player to check
+     * @return <tt>true</tt> if the player is banned, <tt>false</tt> if not.
+     */
     boolean isBanned(PlayerIdentity playerIdentity);
+
+    /**
+     * Gets the ban reason of a banned player.
+     * 
+     * @param playerIdentity Player to get the ban reason for
+     * @return The ban reason, or <tt>null</tt> if the reason is unknown or the
+     *         player is not banned.
+     */
+    String getBanReason(PlayerIdentity playerIdentity);
+
+    /**
+     * Gets the name of the operator that banned the player.
+     * 
+     * @param playerIdentity Player to get the ban operator for
+     * @return The ban operator, or <tt>null</tt> if the operator is unknown or
+     *         the player is not banned.
+     */
+    String getBanOperator(PlayerIdentity playerIdentity);
+
+    /**
+     * Gets the expiration time of the banned player.
+     * 
+     * @param playerIdentity Player to get the ban expiration date for
+     * @return The ban expiration date, or <tt>null</tt> if the ban is permanent
+     *         or the player is not banned.
+     */
+    Timestamp getBanExpiration(PlayerIdentity playerIdentity);
+
 }
--- src/main/java/net/minecrell/serverlistplus/core/player/ban/NoBanDetector.java
+++ src/main/java/net/minecrell/serverlistplus/core/player/ban/NoBanDetector.java
@@ -2,10 +2,27 @@ package net.minecrell.serverlistplus.core.player.ban;
 
 import net.minecrell.serverlistplus.core.player.PlayerIdentity;
 
+import java.sql.Timestamp;
+
 public class NoBanDetector implements BanDetector {
 
     @Override
     public boolean isBanned(PlayerIdentity playerIdentity) {
         return false;
+    }
+
+    @Override
+    public String getBanReason(PlayerIdentity playerIdentity) {
+        return null;
+    }
+
+    @Override
+    public String getBanOperator(PlayerIdentity playerIdentity) {
+        return null;
+    }
+
+    @Override
+    public Timestamp getBanExpiration(PlayerIdentity playerIdentity) {
+        return null;
     }
 }
