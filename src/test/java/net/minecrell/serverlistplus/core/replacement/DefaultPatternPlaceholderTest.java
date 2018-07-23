85b2e0bbd50c6d48422aab39a3d78c623c59ae07
BrainStone <yannick@tedworld.de>
2018-07-23 21:49:49 +0200

    Implemented unit test %random_players% multi separator

---
+++ src/test/java/net/minecrell/serverlistplus/core/replacement/DefaultPatternPlaceholderTest.java
@@ -0,0 +1,47 @@
+/*
+ *        _____                     __    _     _   _____ _
+ *       |   __|___ ___ _ _ ___ ___|  |  |_|___| |_|  _  | |_ _ ___
+ *       |__   | -_|  _| | | -_|  _|  |__| |_ -|  _|   __| | | |_ -|
+ *       |_____|___|_|  \_/|___|_| |_____|_|___|_| |__|  |_|___|___|
+ *
+ *  ServerListPlus - http://git.io/slp
+ *  Copyright (c) 2014, Minecrell <https://github.com/Minecrell>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.minecrell.serverlistplus.core.replacement;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import net.minecrell.serverlistplus.core.status.StatusResponse;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DefaultPatternPlaceholderTest {
+    @Test
+    public void testPlayerList() {
+        final DynamicPlaceholder playerList = DefaultPatternPlaceholder.PLAYER_LIST;
+        final List<String> players = Arrays.asList("Peter", "Steve", "Bob", "Notch", "Hans", "Marry", "Tim", "Herobrine");
+        final String pattern = "%random_players,7|, |. |\n%";
+        final StatusResponse response = mock(StatusResponse.class);
+        
+        when(response.getRandomPlayers()).thenReturn(players.iterator());
+        
+        assertEquals("Peter, Steve. Bob\nNotch, Hans. Marry\nTim", playerList.replace(response, pattern));
+    }
+}
