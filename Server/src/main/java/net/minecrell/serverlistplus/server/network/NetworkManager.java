0f3bab7e59ce3ad8ab93bf04c0cf1faf4bbed5d2
Minecrell <minecrell@minecrell.net>
2018-07-23 20:16:20 +0200

    Get rid of ugly ASCII art and introduce consistent license headers

--- Server/src/main/java/net/minecrell/serverlistplus/server/network/NetworkManager.java
+++ Server/src/main/java/net/minecrell/serverlistplus/server/network/NetworkManager.java
@@ -1,3 +1,21 @@
+/*
+ * ServerListPlus - https://git.io/slp
+ * Copyright (C) 2014 Minecrell (https://github.com/Minecrell)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.minecrell.serverlistplus.server.network;
 
 import static net.minecrell.serverlistplus.server.network.protocol.MinecraftProtocol.PROTOCOL_STATE;
