ef8cda91cc73a4599c359640c4e97dde9b699649
Minecrell <minecrell@minecrell.net>
2020-10-14 11:15:05 +0200

    build.gradle.kts: Use newer AdvancedBan version (v2.2.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     compile("org.ocpsoft.prettytime:prettytime:4.0.1.Final")
 
     compileOnly("org.slf4j:slf4j-api:1.7.25")
-    compileOnly("com.github.DevLeoko:AdvancedBan:4171f1a") { isTransitive = false }
+    compileOnly("com.github.DevLeoko:AdvancedBan:v2.2.1") { isTransitive = false }
 
     testCompile("junit:junit:4.12")
     testCompile("org.mockito:mockito-core:2.20.0")
