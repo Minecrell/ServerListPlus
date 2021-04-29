026b82482071aecff45eda143b39146a3b5b33df
Minecrell <minecrell@minecrell.net>
2021-04-29 17:52:47 +0200

    Velocity/Server: Also allow using Spigot/Bungee format for hex colors
    Instead of &#rrggbb it may be convenient to write &x&r&r&g&g&b&b.
    
    WARNING: I strongly recommend against using the Spigot/Bungee format.
    Doing so may cause serious brain damage!

--- src/main/java/net/minecrell/serverlistplus/core/util/FormattingCodes.java
+++ src/main/java/net/minecrell/serverlistplus/core/util/FormattingCodes.java
@@ -26,7 +26,7 @@ public final class FormattingCodes {
     }
 
     private static final Pattern FORMATTING_CODES = Pattern.compile("§[0-9A-FK-OR]", Pattern.CASE_INSENSITIVE);
-    private static final Pattern CONFIG_CODES_HEX = Pattern.compile("&([0-9A-FK-OR]|#[0-9A-F]{6})", Pattern.CASE_INSENSITIVE);
+    private static final Pattern CONFIG_CODES_HEX = Pattern.compile("&([0-9A-FK-ORX]|#[0-9A-F]{6})", Pattern.CASE_INSENSITIVE);
 
     public static String strip(String s) {
         return FORMATTING_CODES.matcher(s).replaceAll("");
