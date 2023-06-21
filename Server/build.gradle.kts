2e3067198c49b9ab4249eca59fa8caec89db8c62
Minecrell <minecrell@minecrell.net>
2023-06-21 13:00:52 +0200

    Server: Update Netty

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -24,7 +24,7 @@ java {
 }
 
 dependencies {
-    implementation("io.netty:netty-all:4.1.93.Final")
+    implementation("io.netty:netty-all:4.1.94.Final")
 
     implementation("net.minecrell:terminalconsoleappender:1.3.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
